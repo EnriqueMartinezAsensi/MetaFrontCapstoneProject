147261ad4798578e0d02c23ed250e83b5a5d1ce4
EnriqueMartinezAsensi <emartinezasensi@gmail.com>
2024-09-04 17:36:10 +0200

    API work

--- little-lemon/src/App.js
+++ little-lemon/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useReducer } from "react";
+import BookingPage from "./pages/BookingPage";
+
+
+const reducer = (state, action) => {
+  if(action.type === "updateTimes"){
+    return{
+      availableTimes: ["17:00", "18:00","19:00","20:00","21:00","22:00"]
+    }
+  }
+  else if(action.type === "initializeTimes"){
+    return{
+      availableTimes: ["17:00", "18:00","19:00","20:00","21:00","22:00"]
+    }
+  }
+  else throw Error('Unknown action.');
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, {availableTimes:["17:00", "18:00","19:00","20:00","21:00","22:00"]});
+
+  const updateTimes = () => {
+    dispatch({type: "updateTimes"})
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BookingPage availableTimes={state.availableTimes} handleTimes={updateTimes} />
     </div>
   );
 }
--- little-lemon/src/App.test.js
+++ little-lemon/src/App.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Renders the BookingForm heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  const headingElement = screen.getByText("Choose date");
+  expect(headingElement).toBeInTheDocument();
+})
+
+test('initializeTimes initializes the time slot list', () => {
+  render(<App />);
+  const headingElement = screen.getByText("17:00");
+  expect(headingElement).toBeInTheDocument();
+})
+
+test('updateTimes does nothing yet', () => {
+  render(<App />);
+  const headingElement = screen.getByText("21:00");
+  expect(headingElement).toBeInTheDocument();
+})
