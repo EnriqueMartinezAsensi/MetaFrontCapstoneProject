880b6721b1d8bc60d1fb104936ba497debb6098b
EnriqueMartinezAsensi <emartinezasensi@gmail.com>
2024-09-30 12:43:11 +0200

    Start from sctract Vite, typescript, styled components.

--- little-lemon/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('Renders the BookingForm heading', () => {
-  render(<App />);
-  const headingElement = screen.getByText("Choose date");
-  expect(headingElement).toBeInTheDocument();
-})
